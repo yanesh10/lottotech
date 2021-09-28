4055fffb3904654f0c4166f6f2b0b3d14246a835
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2021-09-28 19:10:53 +0400

    Create master

---
+++ lotto-extractor/src/main/java/mu/yanesh/lottoextractor/LottoExtractorApplication.java
@@ -0,0 +1,13 @@
+package mu.yanesh.lottoextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoExtractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoExtractorApplication.class, args);
+    }
+
+}
