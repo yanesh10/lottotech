07b175dc09e9aa09f46acb83fef1a1855c5827a3
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2021-12-09 12:55:25 +0400

    Initial commit for auth server

---
+++ lotto-auth-server/src/main/java/mu/yanesh/lotto/authserver/LottoAuthServerApplication.java
@@ -0,0 +1,13 @@
+package mu.yanesh.lotto.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoAuthServerApplication.class, args);
+    }
+
+}
---
+++ lotto-auth-server/src/test/java/mu/yanesh/lotto/authserver/LottoAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mu.yanesh.lotto.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
