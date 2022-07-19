8cdc5f38124586b366714ea5fab66615104a4e41
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-07-19 21:27:00 +0400

    + Added lotto-analyser

---
+++ lotto-analyser/src/main/java/mu/yanesh/lotto/analyser/LottoAnalyserApplication.java
@@ -0,0 +1,13 @@
+package mu.yanesh.lotto.analyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoAnalyserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoAnalyserApplication.class, args);
+    }
+
+}
---
+++ lotto-analyser/src/test/java/mu/yanesh/lotto/analyser/LottoAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package mu.yanesh.lotto.analyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoAnalyserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
