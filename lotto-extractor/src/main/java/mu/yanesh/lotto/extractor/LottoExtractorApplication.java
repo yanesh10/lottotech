afcbe0fcfaeb781b3c51e4447a67a9a7f5b62a10
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-08-03 15:11:50 +0400

    Configure lotto extractor to eureka

--- lotto-extractor/src/main/java/mu/yanesh/lotto/extractor/LottoExtractorApplication.java
+++ lotto-extractor/src/main/java/mu/yanesh/lotto/extractor/LottoExtractorApplication.java
@@ -2,10 +2,12 @@ package mu.yanesh.lotto.extractor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("mu.yanesh.lotto")
+@EnableDiscoveryClient
 public class LottoExtractorApplication {
 
     public static void main(String[] args) {
