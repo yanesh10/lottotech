05389dde3a54a04d58bdc2a6efc656e5e66686bd
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-08-03 14:14:07 +0400

    Update combinations bean

--- lotto-analyser/src/main/java/mu/yanesh/lotto/analyser/config/CombinationConfig.java
+++ lotto-analyser/src/main/java/mu/yanesh/lotto/analyser/config/CombinationConfig.java
@@ -1,7 +1,9 @@
 package mu.yanesh.lotto.analyser.config;
 
+import lombok.AllArgsConstructor;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import mu.yanesh.lotto.analyser.exception.InvalidCombinationValue;
 import org.paukov.combinatorics3.Generator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,29 +12,25 @@ import org.springframework.core.env.Environment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Configuration
 @Slf4j
 public class CombinationConfig {
 
-    @Autowired
-    private Environment env;
-
     @Bean
-    public List<List<Integer>> combinations() {
+    public List<List<Integer>> combinations(@Autowired Environment env) {
         log.info("Creating combination bean");
         List<List<Integer>> combinations = new ArrayList<>();
         List<Integer> numbers = new ArrayList<>();
         for (int i = 1; i <= 40; i++) {
             numbers.add(i);
         }
-        int combination = env.getProperty("combinations", Integer.class);
+        int combination =
+                Optional.ofNullable(env.getProperty("combinations", Integer.class))
+                        .orElseThrow(InvalidCombinationValue::new);
 
         combinations.addAll(Generator.combination(numbers).simple(combination).stream().toList());
-//        combinations.addAll(Generator.combination(numbers).simple(3).stream().toList());
-//        combinations.addAll(Generator.combination(numbers).simple(4).stream().toList());
-//        combinations.addAll(Generator.combination(numbers).simple(5).stream().toList());
-//        combinations.addAll(Generator.combination(numbers).simple(6).stream().toList());
 
         log.info("Combination bean created with {}", combinations.size());
 
---
+++ lotto-analyser/src/main/java/mu/yanesh/lotto/analyser/exception/InvalidCombinationValue.java
@@ -0,0 +1,8 @@
+package mu.yanesh.lotto.analyser.exception;
+
+public class InvalidCombinationValue extends RuntimeException {
+
+    public InvalidCombinationValue() {
+        super("Invalid Combinations");
+    }
+}
