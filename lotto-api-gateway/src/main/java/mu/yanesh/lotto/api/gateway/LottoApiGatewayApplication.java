9e471b271ec8f3e2e4a8dcab13e83a1fb66c5132
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-08-03 11:03:46 +0400

    Configure lotto-api-gateway as eureka client

--- lotto-api-gateway/src/main/java/mu/yanesh/lotto/api/gateway/LottoApiGatewayApplication.java
+++ lotto-api-gateway/src/main/java/mu/yanesh/lotto/api/gateway/LottoApiGatewayApplication.java
@@ -2,8 +2,10 @@ package mu.yanesh.lotto.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LottoApiGatewayApplication {
 
     public static void main(String[] args) {
