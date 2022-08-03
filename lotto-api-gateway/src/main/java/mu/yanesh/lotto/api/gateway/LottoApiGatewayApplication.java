b67696975bca5800b20a0a2f5514011eba5af4c7
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-08-03 10:31:16 +0400

    Added API Gateway

---
+++ lotto-api-gateway/src/main/java/mu/yanesh/lotto/api/gateway/LottoApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package mu.yanesh.lotto.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoApiGatewayApplication.class, args);
+    }
+
+}
---
+++ lotto-api-gateway/src/test/java/mu/yanesh/lotto/api/gateway/LottoApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mu.yanesh.lotto.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
